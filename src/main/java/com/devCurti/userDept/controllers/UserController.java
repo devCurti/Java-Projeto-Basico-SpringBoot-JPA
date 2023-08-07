02b0d31206255ae2e769e3f0a71c64c35be71d02
devCurti <joaocurti2002@gmail.com>
2023-08-07 13:29:26 -0300

    Update UserController.java

--- src/main/java/com/devCurti/userDept/controllers/UserController.java
+++ src/main/java/com/devCurti/userDept/controllers/UserController.java
@@ -34,7 +34,7 @@ public class UserController {
 	
 	@PostMapping
 	public User insert(@RequestBody User user) {
-		User result = repository.findById(id).get();
+		User result = repository.save(user);
 		return result;
 	}
 }
